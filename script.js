05ba2eaeedd61911d865c96b1211f25aba063686
Isun ODON <115283479+IsunODON@users.noreply.github.com>
2023-07-09 00:42:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// text animation script
+var typed = new Typed(".typing", {
+    strings: ["Manusia Biasa :)"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    cursorChar: ' | ',
+    smartBackspace: true,
+    loop: true
+});
